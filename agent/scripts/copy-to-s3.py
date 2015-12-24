78f142f65a727d0d6b9d8cca0bb81f177ff66e6b
Michael Yakobi <yakobi@netapp.com>
2015-12-24 16:13:20 +0200

    First working versoin of copy-to-s3 script

--- agent/scripts/copy-to-s3.py
+++ agent/scripts/copy-to-s3.py
@@ -8,21 +8,21 @@ import re
 import tempfile
 
 def usage():
-  print "Usage: " + sys.argv[0] + " --source-nfs-url <nfs://source-ip/path> --target-s3-url <s3://bucket/path> [--refresh] [--sns-topic <sns-topic-arn>]"
+  print "Usage: " + sys.argv[0] + " --copy-id <copy-session-id> --source-nfs-url <nfs://source-ip/path> --target-s3-url <s3://bucket/path> [--refresh] [--sns-topic <sns-topic-arn>]"
   sys.exit(2)
 
 try:
-  opts, args = getopt.getopt(sys.argv[1:], "s:t:rn:", ["source-nfs-url=", "target-s3-url=", "refres", "sns-topic="])
+  opts, args = getopt.getopt(sys.argv[1:], "i:s:t:rn:", ["copy-id=", "source-nfs-url=", "target-s3-url=", "refres", "sns-topic="])
 except getopt.GetoptError:
   usage()
 
-sourceNfsUrl = targetS3Url = snsTopic = None
+copyId = sourceNfsUrl = targetS3Url = snsTopic = None
 refresh = False
 
 for opt, arg in opts:
   if opt in ("-a", "--source-nfs-url"):
     sourceNfsUrl = arg
-  elif opt in ("-u", "--target-s3-url"):
+  elif opt in ("-t", "--target-s3-url"):
     targetS3Url = arg
   elif opt in ("-r", "--refresh"):
     refresh = true
@@ -42,7 +42,7 @@ os.system("mount " + nfsSource + " " + mountDir + " -o nolock")
 print "Mounted on: " + mountDir
 
 print "Starting to copy to: " + targetS3Url
-os.system("s3cmd sync " + mountDir + " " + targetS3Url)
+os.system("s3cmd sync " + mountDir + " " + targetS3Url + " --exclude '.snapshot/*'")
 print "Copy completed"
 
 os.system("umount " + mountDir)
@@ -50,4 +50,4 @@ os.rmdir(mountDir)
 print "Unmounted from: " + mountDir
 
 if snsTopic is not None:
-  os.system("aws sns publish --region " + metadata.region + " --topic-arn " + snsTopic + " --subject copy-to-s3 --message '{\"instance-id\": \"" + metadata.instanceId + "\", \"subnet-id\": \"" + metadata.subnet +"\", \"copy-completed\": { \"cluster-mgmt-ip\": \"" + metadata.address + "\" }}'")
+  os.system("aws sns publish --region " + metadata.region + " --topic-arn " + snsTopic + " --subject copy-to-s3 --message '{\"copyId\": \"" + copyId + "\", \"instance-id\": \"" + metadata.instanceId + "\", \"subnet-id\": \"" + metadata.subnet +"\", \"copy-completed\": { \"cluster-mgmt-ip\": \"" + metadata.address + "\" }}'")
