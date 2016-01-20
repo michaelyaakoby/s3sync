89754431bdf2bd949998b1e13bf87c6afe0fcf06
Michael Yakobi <yakobi@netapp.com>
2016-01-20 18:03:17 +0200

    change buckets in the reset script

--- demo/reset-xcp-and-bucket.sh
+++ demo/reset-xcp-and-bucket.sh
@@ -4,9 +4,7 @@ rm -f /opt/NetApp/xFiles/xcp/license
 cp -f /xcp-license.xwic /opt/NetApp/xFiles/xcp/license
 rm -rf /catalog/*
 
-xcp diag -rmrf s3://ontap-sync
-xcp diag -rmrf s3://ontap-sync-standard
-xcp diag -rmrf s3://tami-bucket
-xcp diag -rmrf s3://qa-bucket1
-xcp diag -rmrf s3://marketing-results
-xcp diag -rmrf s3://analytics-bucket
+xcp diag -rmrf s3://tlv-results
+xcp diag -rmrf s3://tlv-marketing-results
+xcp diag -rmrf s3://tlv-analytics
+xcp diag -rmrf s3://tlv-qa
