46a6c6791ad9fe263e18a8996a229b890bae4c79
Michael Yakobi <yakobi@netapp.com>
2016-02-24 12:34:31 +0200

    updated xcp to build 23

--- demo/reset-xcp-and-bucket.sh
+++ demo/reset-xcp-and-bucket.sh
@@ -4,7 +4,7 @@ rm -f /opt/NetApp/xFiles/xcp/license
 cp -f /xcp-license.xwic /opt/NetApp/xFiles/xcp/license
 rm -rf /catalog/*
 
-python -c "import boto3; c = boto3.client('sns').publish(TopicArn='arn:aws:sns:us-west-2:718273455463:occmservice', Subject='reset-demo', Message='Pppplease')"
+python -c "import boto3; c = boto3.client('sns').publish(TopicArn='arn:aws:sns:us-west-2:718273455463:occmservice', Subject='reset-demo', Message='{}')"
 
 xcp diag -rmrf s3://tlv-results
 xcp diag -rmrf s3://tlv-marketing-results
